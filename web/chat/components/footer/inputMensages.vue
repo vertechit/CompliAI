bea49caf142beabcbe62d730d0ca35e07e20e93b
FelipeAllue <felipeoallue@gmail.com>
2024-05-07 20:10:05 -0300

    Inicio do desenvolvimento dos arquivos em enexo

--- web/chat/components/footer/inputMensages.vue
+++ web/chat/components/footer/inputMensages.vue
@@ -6,9 +6,13 @@
       </div>
       <div class="min-w-0 flex-1">
         <form action="#" @submit.prevent>
-          <div class="border-b border-gray-200 focus-within:border-indigo-600">
+          <div class="border-b border-gray-200 focus-within:border-blue-600">
             <label for="comment" class="sr-only">Escreva sua dúvida</label>
-            <textarea  v-model="message" @keypress.enter="onSubmit" rows="3" name="comment" id="comment" class="block w-full resize-none border-0 border-b border-transparent p-0 pb-2 text-gray-900 placeholder:text-gray-400 focus:border-indigo-600 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Envie sua Dúvida" />
+            <textarea :disabled="loading" v-model="message" @keypress.enter="onSubmit" rows="3" name="comment" id="comment" 
+             class="block w-full resize-none border-0 border-b border-transparent p-0 pb-2 text-gray-900 placeholder:text-gray-400 focus:border-blue-600 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Envie sua Dúvida"
+            :class="{'cursor-not-allowed bg-gray-100':loading}" 
+            
+            />
           </div>
           <div class="flex justify-between pt-2">
             <div class="flex items-center space-x-5">
@@ -17,7 +21,9 @@
               </div>
             </div>
             <div class="flex-shrink-0">
-              <button type="submit" @keypress.enter="onSubmit"  class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Enviar</button>
+              <button :disable="loading" type="submit" @keypress.enter="onSubmit"  class="inline-flex items-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+              :class="{'cursor-not-allowed bg-blue-800':loading}" 
+              >Enviar</button>
             </div>
           </div>
         </form>
@@ -26,8 +32,11 @@
   </template>
   
   <script setup lang="ts">
+  import {defaultStore} from '@/stores/default'
+  const defaultStorePinia = defaultStore()
   let message = ref('')
   const emit = defineEmits(['submit'])
+  const loading = computed(()=>defaultStorePinia.loading)
   function onSubmit() {
     emit('submit', message.value)
     message.value = ''
--- web/chat/components/sideBar/sideBar.vue
+++ web/chat/components/sideBar/sideBar.vue
@@ -17,16 +17,16 @@
                     </button>
                   </div>
                 </TransitionChild>
-                <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-2 ring-1 ring-white/10">
-                  <div class="flex h-16 shrink-0 items-center">
-                    <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+                <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-blue-900 px-6 pb-2 ring-1 ring-white/10">
+                  <div class="flex h-32 shrink-0 items-center">
+                    <img class="h-8 w-auto" src="~/assets/img/logoCompliIc.png" alt="Compliance Soluções" />
                   </div>
                   <nav class="flex flex-1 flex-col">
                     <ul role="list" class="flex flex-1 flex-col gap-y-7">
                       <li>
                         <ul role="list" class="-mx-2 space-y-1">
                           <li v-for="item in navigation" :key="item.name">
-                            <a :href="item.href" :class="[item.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
+                            <a :href="item.href" :class="[item.current ? 'bg-blue-800 text-white' : 'text-white hover:text-white hover:bg-blue-800', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
                               <component :is="item.icon" class="h-6 w-6 shrink-0" aria-hidden="true" />
                               {{ item.name }}
                             </a>
@@ -34,11 +34,11 @@
                         </ul>
                       </li>
                       <li>
-                        <div class="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
+                        <div class="text-xs font-semibold leading-6 text-white">Your teams</div>
                         <ul role="list" class="-mx-2 mt-2 space-y-1">
                           <li v-for="team in teams" :key="team.name">
-                            <a :href="team.href" :class="[team.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
-                              <span class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">{{ team.initial }}</span>
+                            <a :href="team.href" :class="[team.current ? 'bg-blue-800 text-white' : 'text-white hover:text-white hover:bg-blue-800', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
+                              <span class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-blue-700 bg-blue-800 text-[0.625rem] font-medium text-white group-hover:text-white">{{ team.initial }}</span>
                               <span class="truncate">{{ team.name }}</span>
                             </a>
                           </li>
@@ -53,17 +53,17 @@
         </Dialog>
       </TransitionRoot>
   
-      <div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-        <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
-          <div class="flex h-16 shrink-0 items-center">
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+      <div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-64 lg:flex-col ">
+        <div class="flex grow flex-col gap-y-5 overflow-y-auto p-1 bg-blue-900 px-6 rounded-r-xl" >
+          <div class="flex h-14 shrink-0 items-center">
+            <img class="w-auto h-full" src="~/assets/img/logoCompliIc.png" alt="Compliance Soluções" />
           </div>
           <nav class="flex flex-1 flex-col">
             <ul role="list" class="flex flex-1 flex-col gap-y-7">
               <li>
                 <ul role="list" class="-mx-2 space-y-1">
                   <li v-for="item in navigation" :key="item.name">
-                    <a :href="item.href" :class="[item.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
+                    <a :href="item.href" :class="[item.current ? 'bg-blue-800 text-white' : 'text-white hover:text-white hover:bg-blue-800', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
                       <component :is="item.icon" class="h-6 w-6 shrink-0" aria-hidden="true" />
                       {{ item.name }}
                     </a>
@@ -71,19 +71,19 @@
                 </ul>
               </li>
               <li>
-                <div class="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
+                <div class="text-xs font-semibold leading-6 text-white">Your teams</div>
                 <ul role="list" class="-mx-2 mt-2 space-y-1">
                   <li v-for="team in teams" :key="team.name">
-                    <a :href="team.href" :class="[team.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
-                      <span class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">{{ team.initial }}</span>
+                    <a :href="team.href" :class="[team.current ? 'bg-blue-800 text-white' : 'text-white hover:text-white hover:bg-blue-800', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
+                      <span class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-blue-700 bg-blue-800 text-[0.625rem] font-medium text-white group-hover:text-white">{{ team.initial }}</span>
                       <span class="truncate">{{ team.name }}</span>
                     </a>
                   </li>
                 </ul>
               </li>
               <li class="-mx-6 mt-auto">
-                <a href="#" class="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white hover:bg-gray-800">
-                  <img class="h-8 w-8 rounded-full bg-gray-800" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                <a href="#" class="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white hover:bg-blue-800">
+                  <img class="h-8 w-8 rounded-full bg-blue-800" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                   <span class="sr-only">Your profile</span>
                   <span aria-hidden="true">Bete</span>
                 </a>
@@ -93,15 +93,15 @@
         </div>
       </div>
   
-      <div class="sticky top-0 z-40 flex items-center gap-x-6 bg-gray-900 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+      <div class="sticky top-0 z-40 flex items-center gap-x-6 bg-blue-900 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
         <button type="button" class="-m-2.5 p-2.5 text-gray-400 lg:hidden" @click="sidebarOpen = true">
           <span class="sr-only">Open sidebar</span>
           <Bars3Icon class="h-6 w-6" aria-hidden="true" />
         </button>
-        <div class="flex-1 text-sm font-semibold leading-6 text-white">Dashboard</div>
+        <div class="flex-1 text-sm font-semibold leading-6 text-white">Chat</div>
         <a href="#">
           <span class="sr-only">Your profile</span>
-          <img class="h-8 w-8 rounded-full bg-gray-800" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+          <img class="h-8 w-8 rounded-full bg-blue-800" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
         </a>
       </div>
   
@@ -123,14 +123,14 @@
     ChartPieIcon,
     DocumentDuplicateIcon,
     FolderIcon,
-    HomeIcon,
-    UsersIcon,
+    DocumentTextIcon,
     XMarkIcon,
+    ChatBubbleOvalLeftEllipsisIcon
   } from '@heroicons/vue/24/outline'
   
   const navigation = [
-    { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-    { name: 'Team', href: '#', icon: UsersIcon, current: false },
+    { name: 'Chat', href: '/', icon: ChatBubbleOvalLeftEllipsisIcon, current: true },
+    { name: 'Documentos', href: '/documentos', icon: DocumentTextIcon, current: false },
     { name: 'Projects', href: '#', icon: FolderIcon, current: false },
     { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
     { name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false },
