1e653e7a56661e702c80a58e041b07afb5cae05d
Lucas Souza <lucassampsouza@gmail.com>
2024-05-08 10:42:21 -0300

    criado validador de email

--- controllers/UserController.py
+++ controllers/UserController.py
@@ -1,12 +1,27 @@
 from models.User import User
 import hashlib
+import re
+
+def validar_email(email)-> bool:
+    regexMail = re.compile(r"([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])")
+    if re.match(regexMail, email):
+        return True
+    else:
+        raise ValueError("O login precisa ser um email válido")
 
 def login(login: str, password: str)-> int:
+    retorno = None
     user = User.select().where((User.login==login.lower()) & (User.senha == hashlib.sha256(password.encode()).hexdigest())).first()
-    return user
+    if user:
+        retorno = user.user_id
+    return retorno
 
 def create_user(login: str, password: str)-> int:
+    retorno = None
+    validar_email(login)
     criptoSenha = hashlib.sha256(password.encode()).hexdigest()
     user = User(login=login.lower(), senha=criptoSenha, tipo=0)
     user.save()
-    return user
+    if user:
+        retorno = user.user_id
+    return retorno
