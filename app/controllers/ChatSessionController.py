3acdaf3e055cb4ebac2173adfcdb1810ac7beb02
arthur <arthur.c.mendes@hotmail.com>
2024-06-02 19:37:30 -0300

    Aleterar condição

--- app/controllers/ChatSessionController.py
+++ app/controllers/ChatSessionController.py
@@ -1,37 +1,33 @@
-#CompliAi - Issue 7
 from models.ChatHistory import ChatSession
 from typing import List
 from llm import llm
 
-
-def saveSessao(pergunta: str, session_id: int):
-    session = None
-    session = ChatSession.select().where(ChatSession.session_id == session_id)
-    if session == None:
+def save_sessao(pergunta: str, session_id: int):
+    session = ChatSession.select().where(ChatSession.session_id == session_id).first()
+    if session is None:
         titulo = llm.chain_titulo(pergunta)
         session = ChatSession(session_id=session_id, titulo=titulo)
         session.save()
     else:
         pass
 
-def deleteSessao(session_id: int)-> str:
+def delete_sessao(session_id: int) -> str:
     session = ChatSession.select().where(ChatSession.session_id == session_id).first()
-    if session == None:
+    if session is None:
         return "Sessão não encontrada"
     else:
         ChatSession.delete().where(ChatSession.session_id == session_id).execute()
         return "Sessão deletada"
 
-def listSessao(session_id: int = None)-> List[ChatSession]:
-    sessions = None
+def list_sessao(session_id: int = None) -> List[ChatSession]:
     array = []
-    if session_id == None:
+    if session_id is None:
         sessions = ChatSession.select()
     else:
         sessions = ChatSession.select().where(ChatSession.session_id == session_id)
-    for session in sessions:
-        newSession = (session.session_id, session.titulo, session.criado)
-        array.append(newSession)
-    return array
-
     
+    for session in sessions:
+        new_session = (session.session_id, session.titulo, session.criado)
+        array.append(new_session)
+    
+    return array
