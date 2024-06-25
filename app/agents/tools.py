cc3096f28ea10eb25582923020143c4c04e44986
Lucas Souza <lucassampsouza@gmail.com>
2024-06-25 10:30:14 -0300

    Alterado descrição da Tool do Retriever

--- app/agents/tools.py
+++ app/agents/tools.py
@@ -4,7 +4,7 @@ from vectors.vectorStore import getRetriever
 retriever_tool = create_retriever_tool(
     getRetriever(),
     "retrieve_documents",
-    "Retorna informações sobre documentos gerais sobre assuntos Fiscais e de Folha de pagamento"
+    "Retorna informações de documentos sobre assuntos Fiscais e de Folha de pagamento"
 )
 
 tools = [retriever_tool]
